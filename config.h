5154ed6018d70515301f190a80b38a8c3d658cf8
FdLSifu <hamzin@gmail.com>
2019-11-20 00:21:26 +0100

    alpha diff + colors

--- config.h
+++ config.h
@@ -6,6 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Ubuntu Mono:pixelsize=20:antialias=true:autohint=true";
+
 static int borderpx = 2;
 
 /*
@@ -82,8 +83,11 @@ char *termname = "st-256color";
  */
 unsigned int tabspaces = 8;
 
+/* bg opacity */
+float alpha = 0.9;
+
 /* Terminal colors (16 first used in escape sequence) */
-static const char *colorname[] = {
+static const char *colorname_orig[] = {
 	/* 8 normal colors */
 	"black",
 	"red3",
@@ -109,17 +113,53 @@ static const char *colorname[] = {
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#cccccc",
 	"#555555",
+	"black",
 };
 
+/* Terminal colors (16 first used in escape sequence) */
+static const char *colorname[] = {
+
+	/* 8 normal colors */
+	[0] = "#151515", /* black   */
+	[1] = "#ac4142", /* red     */
+	[2] = "#90a959", /* green   */
+	[3] = "#f4bf75", /* yellow  */
+	[4] = "#6a9fb5", /* blue    */
+	[5] = "#aa759f", /* magenta */
+	[6] = "#75b5aa", /* cyan    */
+	[7] = "#d0d0d0", /* white   */
+
+	/* 8 bright colors */
+	[8]  = "#505050", /* black   */
+	[9]  = "#ac4142", /* red     */
+	[10] = "#90a959", /* green   */
+	[11] = "#f4bf75", /* yellow  */
+	[12] = "#6a9fb5", /* blue    */
+	[13] = "#aa759f", /* magenta */
+	[14] = "#75b5aa", /* cyan    */
+	[15] = "#f5f5f5", /* white   */
+
+	/* special colors */
+	[256] = "#151515", /* background */
+	[257] = "#d0d0d0", /* foreground */
+};
 
 /*
- * Default colors (colorname index)
- * foreground, background, cursor, reverse cursor
- */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
-static unsigned int defaultcs = 256;
+ *  * Default colors (colorname index)
+ *   * foreground, background, cursor, reverse cursor
+ *    */
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+static unsigned int defaultcs = 257;
 static unsigned int defaultrcs = 257;
+
+/*
+ *  * Colors used, when the specific fg == defaultfg. So in reverse mode this
+ *   * will reverse too. Another logic would only make the simple feature too
+ *    * complex.
+ *     */
+static unsigned int defaultitalic = 7;
+static unsigned int defaultunderline = 7;
 
 /*
  * Default shape of cursor
